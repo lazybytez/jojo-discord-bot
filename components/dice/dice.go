77300fdd82fbf85fe7b57f77a35a2ecc0f0e5328
Michael Vogel <michael.vogel@flagbit.de>
2022-08-11 14:20:33 +0200

    fix(comp): adjust dice function
    Refs: #38

--- components/dice/dice.go
+++ components/dice/dice.go
@@ -6,12 +6,12 @@ import (
 
 // rollDice rolls `n` dice where the dice have each `d` sites and return the results as an int-array.
 // For example: If you want to throw 3 times a d6, then `d` should be 6 and `n` should be 3.
-func rollDice(d int, n int) {
-	r := [n]int;
+func rollDice(d int, n int) []int {
+	var r []int
 
 	for i := 1; i <= n; i++ {
-		r[i] = rand.Intnl(d);
+		r[i] = rand.Intn(d - 1) + 1
 	}
 
-	return r;
+	return r
 }
