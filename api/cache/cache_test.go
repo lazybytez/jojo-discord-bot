cb75a395b3d4f3d3964b576657e53a58a64e9849
Pascal Zarrad <p.zarrad@outlook.de>
2022-08-14 01:02:11 +0200

    test(api): add tests for auto cache cleanup

--- api/cache/cache_test.go
+++ api/cache/cache_test.go
@@ -92,6 +92,52 @@ func TestGet(t *testing.T) {
 	}
 }
 
+func TestCache_EnableAutoCleanup(t *testing.T) {
+	str := "some value"
+
+	tables := []struct {
+		key          string
+		value        *string
+		lifetime     time.Duration
+		delaySeconds time.Duration
+		expectedOK   bool
+	}{
+		{"test1", &str, 0, 0, true},
+		{"test2", &str, 0, 2, true},
+		{"test3", &str, 3, 1, true},
+		{"test4", &str, 1, 2, false},
+	}
+
+	for _, table := range tables {
+		duration := table.lifetime * time.Second
+
+		testCache := &Cache[string, string]{
+			map[string]*Item[*string]{},
+			sync.RWMutex{},
+			duration.Milliseconds(),
+			nil,
+		}
+		testCache.EnableAutoCleanup(500 * time.Millisecond)
+
+		testCache.cache[table.key] = &Item[*string]{
+			table.value,
+			time.Now(),
+			time.Now(),
+		}
+
+		time.Sleep(table.delaySeconds * time.Second)
+
+		if _, ok := testCache.cache[table.key]; ok != table.expectedOK {
+			t.Errorf("output of cache get with key \"%v\" and "+
+				"value \"%v\" was incorrect, got: %v, want: %v.",
+				table.key,
+				table.value,
+				ok,
+				table.expectedOK)
+		}
+	}
+}
+
 func TestUpdate(t *testing.T) {
 	const testKey = "test"
 	testDataA := "A"
